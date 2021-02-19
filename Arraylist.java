9bbfadd2767cdb1545b5464a6f3471024317e919
Samson-coder <78577953+Samson-coder@users.noreply.github.com>
2021-02-19 17:02:25 +0530

    Create Arraylist.java

---
+++ Arraylist.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Collection{
+    public static void main(String[] args){
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        values.add(10);
+        values.add(20);
+        values.add(30);
+        values.add(40);
+        System.out.println(values);
+    }
+}
